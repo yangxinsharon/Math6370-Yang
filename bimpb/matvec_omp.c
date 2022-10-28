303454fba56fe18cb177c48366a872bcda3e0fd1
Sharon Yang <xiny@smu.edu>
2022-10-28 11:09:07 -0500

    Update matvec_omp.c

--- bimpb/matvec_omp.c
+++ bimpb/matvec_omp.c
@@ -42,6 +42,10 @@ void matvecmul(const double *x, double *y, double *q, int nface,
     // #pragma omp parallel for shared(nface,tr_xyz,tr_q,one_over_4pi,kappa,x,y,alpha,beta,pre1,pre2)
     #pragma omp parallel default
     {
+    	printf(“The parallel region is executed by thread %i\n”, omp_get_thread_num());
+		// if ( omp_get_thread_num() == 2 )
+		// 	printf(“ thread %i does things differently\n”, omp_get_thread_num());
+
     	int i, j, tmp_i, tmp_j, tid, nthreads;
 
     	#pragma omp parallel for 
