564e9604b2cdcc1fe1926a992f253554575d1c49
Sharon Yang <xiny@smu.edu>
2022-11-17 11:32:03 -0600

    Update main_mpi.c

--- bimpb/main_mpi.c
+++ bimpb/main_mpi.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
 	gmres_(&N, bvct, xvct, &RESTRT, work, &ldw, h, &ldh, &iter, &resid, &matvec, &psolve, &info);
 	
 	// /* finalize MPI */
-	ierr = MPI_Finalize();
+	int ierr = MPI_Finalize();
 
 	soleng=0.0;
 
