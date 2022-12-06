dcd1e8d3aee726f6dda3b6cbd9095b32b7368619
Sharon Yang <xiny@smu.edu>
2022-12-05 20:05:59 -0600

    Update main_kokkos.cpp

--- bimpb/kokkos/main_kokkos.cpp
+++ bimpb/kokkos/main_kokkos.cpp
@@ -32,7 +32,9 @@ int main(int argc, char *argv[]) {
 	double s[3], pot=0.0, sum=0.0, pot_temp=0.0;	// potential
 	double ptl, soleng, t1, t2;
 	char fname[16], density[16];
-	extern "C" void readin(char fname[16], char density[16]);
+	extern "C" {
+	void readin(char fname[16], char density[16]);
+	}
 
 	/*GMRES related variables*/
 	static long int info;
@@ -56,7 +58,7 @@ int main(int argc, char *argv[]) {
    sprintf(density, "1");
    // sprintf(fname,"%s",argv[1]);
    // sprintf(density,"%s",argv[2]);
-	readin(fname, density);
+	extern "C" readin(fname, density);
 	comp_source_wrapper(); //wraps the solvation energy computation
 
 	/* parameters for GMRES */
