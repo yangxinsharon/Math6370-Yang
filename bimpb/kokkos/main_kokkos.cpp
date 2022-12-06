a998ed702d2c045bd6bb57ae9bf5bac704ea6b0d
Sharon Yang <xiny@smu.edu>
2022-12-05 20:00:24 -0600

    update

--- bimpb/kokkos/main_kokkos.cpp
+++ bimpb/kokkos/main_kokkos.cpp
@@ -32,10 +32,8 @@ int main(int argc, char *argv[]) {
 	double s[3], pot=0.0, sum=0.0, pot_temp=0.0;	// potential
 	double ptl, soleng, t1, t2;
 	char fname[16], density[16];
-	extern "C" {
-		void readin(char fname[16], char density[16]);
-	}
-	
+	extern "C" void readin(char fname[16], char density[16]);
+
 	/*GMRES related variables*/
 	static long int info;
 	long int RESTRT, ldw, ldh, iter, N;
