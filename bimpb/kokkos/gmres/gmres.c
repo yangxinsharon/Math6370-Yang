8f82c8bbef9c3367922150d1337af1c9ba208434
Sharon Yang <xiny@smu.edu>
2022-12-06 23:03:17 -0600

    Update gmres.c

--- bimpb/kokkos/gmres/gmres.c
+++ bimpb/kokkos/gmres/gmres.c
@@ -127,7 +127,7 @@ int gmres_(n, b, x, restrt, work, ldw, h, ldh, iter, resid, matvec, psolve,
            info)
    integer *n, *restrt, *ldw, *ldh, *iter, *info;
    doublereal *b, *x, *work, *h, *resid;
-   int (*matvec) (), (*psolve) ();
+   int (*matvec) (doublereal *alpha, doublereal *x, doublereal *beta, doublereal *y), (*psolve) ();
 {
 
     /* System generated locals */
