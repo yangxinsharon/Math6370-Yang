01d3dbb8566016708bfdccd2e004d9f338d0fc53
Sharon Yang <xiny@smu.edu>
2022-12-06 22:44:10 -0600

    Update main_kokkos.cpp

--- bimpb/kokkos/main_kokkos.cpp
+++ bimpb/kokkos/main_kokkos.cpp
@@ -40,8 +40,8 @@ extern "C"
 // int add(const int a, const int b);
 int *matvec ();
 int *psolve ();
-int gmres_(long int *n, double *b, double *x, long int *restrt, double *work, long int *ldw, 
-		double *h, long int *ldh, long int *iter, double *resid, int *matvec (), int *psolve (), long int *info);
+// int gmres_(long int *n, double *b, double *x, long int *restrt, double *work, long int *ldw, 
+// 		double *h, long int *ldh, long int *iter, double *resid, int *matvec (), int *psolve (), long int *info);
 
 
 #ifdef __cplusplus
